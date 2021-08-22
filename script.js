c68c42693dd8114a96b4076ec60ccf1a8da32dc3
Devin <ospreywebservices@gmail.com>
2021-08-21 20:23:06 -0400

    added images in divs, fixed style layout, starting js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log('worked');
+let clock = document.getElementById('clock');
+let phone = document.getElementById('phone');
+let group = document.getElementById('group');
+let hamburger = document.getElementById('hamburger');
+let profile = document.getElementById('profile');
